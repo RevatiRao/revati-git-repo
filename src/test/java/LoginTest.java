1280b5c60dc80bc85a4a57fdc956fab6d537bfa3
Amaan <m.amaanraza@gmail.com>
2020-03-05 01:32:18 -0500

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp()
+	{
+	WebDriverManager.firefoxdriver().setup();	
+	driver = new FirefoxDriver();
+	
+	}
+	@Test
+	public void doLogin() throws InterruptedException 
+	{
+		
+		driver.get("http://gmail.com/");
+		Thread.sleep(8000);
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("revatidrao222@gmail.com");
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span")).click();
+		Thread.sleep(8000);
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")).sendKeys("dhawal92");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+	driver.quit();
+	}
+	
+}
